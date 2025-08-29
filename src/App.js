9f4ffe55d38aca84f460fcde3421c10784303367
Isradias <israelcaldas10@gmail.com>
2025-08-29 08:48:16 -0300

    HashRouter no lugar de  BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { HashRouter, Routes, Route} from "react-router-dom"
 import Home from './pages/Home/Home.js'
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
